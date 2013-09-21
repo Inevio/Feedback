fe83c927193724010e7047fb8df1e4cebb5a1589
Robby2012 <robby.upm@gmail.com>
2013-09-21 12:06:48 +0200

    Apps - Converted tabs into spaces

--- static/script.js
+++ static/script.js
@@ -1,42 +1,42 @@
 
-	$( win )
+    $( win )
 
-		.on( 'click', '.content-button', function(){
+    .on( 'click', '.content-button', function(){
 
-			if( $( '.content-subject-text', win ).val() && $( '.content-description-text', win ).val() ){
+        if( $( '.content-subject-text', win ).val() && $( '.content-description-text', win ).val() ){
 
-				wz.contact( $( '.content-subject-text', win ).val(), $( '.content-description-text', win ).val(), function( error ){
+            wz.contact( $( '.content-subject-text', win ).val(), $( '.content-description-text', win ).val(), function( error ){
 
-					if( error ){
-						console.log( error );
-					}
+                if( error ){
+                    console.log( error );
+                }
 
-				});
+            });
 
-				wz.banner()
-					.title( lang.bannerTitle )
-					.text( lang.bannerDescription )
-					.icon( 'https://static.weezeel.com/app/9/icon.png' )
-					.render();
+            wz.banner()
+                .title( lang.bannerTitle )
+                .text( lang.bannerDescription )
+                .icon( 'https://static.weezeel.com/app/9/icon.png' )
+                .render();
 
-				wz.app.closeWindow( win );
+            wz.app.closeWindow( win );
 
-			}else{
+        }else{
 
-				if( !$( '.content-subject-text', win ).val() && !$( '.content-description-text', win ).val() ){
-					alert( lang.errorTotal, null, win.data().win );
-				}else if( !$( '.content-subject-text', win ).val() ){
-					alert( lang.errorSubject, null, win.data().win );
-				}else{
-					alert( lang.errorDescription, null, win.data().win );
-				}
+            if( !$( '.content-subject-text', win ).val() && !$( '.content-description-text', win ).val() ){
+                alert( lang.errorTotal, null, win.data().win );
+            }else if( !$( '.content-subject-text', win ).val() ){
+                alert( lang.errorSubject, null, win.data().win );
+            }else{
+                alert( lang.errorDescription, null, win.data().win );
+            }
 
-			}
+        }
 
-		});
+    });
 
-	$( '.contact-title', win ).text( lang.contactTitle );
-	$( '.contact-info', win ).text( lang.contactInfo );
-	$( '.contact-subject', win ).text( lang.contactSubject + ':' );
-	$( '.contact-description', win ).text( lang.contactDescription + ':' );
-	$( '.content-button span', win ).text( lang.contactSend );
+    $( '.contact-title', win ).text( lang.contactTitle );
+    $( '.contact-info', win ).text( lang.contactInfo );
+    $( '.contact-subject', win ).text( lang.contactSubject + ':' );
+    $( '.contact-description', win ).text( lang.contactDescription + ':' );
+    $( '.content-button span', win ).text( lang.contactSend );
